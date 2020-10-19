987b6e767446028b28e1629c31c17ecd9246cff0
apla02 <1873@holbertonschool.com>
2020-10-19 16:14:22 -0500

    quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * swap - swap two elements of array
  * @i: pointer to element 1
@@ -13,69 +12,61 @@ void swap(int *i, int *j)
 	*i = *j;
 	*j = temp;
 }
-
 /**
  * partition - Process where the array is organized.
  * @array: Data structure.
- * @lower: Index lower
+ * @low: Index low
  * @high: Index high
  * @size: size array
  * Return: Index
  */
-int partition(int *array, int *lower, int *high, int size)
+int partition(int *array, int low, int high, size_t size)
 {
-	int *Pivote = high;
-	int *j = lower;
-	int *i = lower;
+	int pivot = array[high], i = low - 1, j = low;
 
-	while (j < high)
+	while (j <= high - 1)
 	{
-		if (*j < *Pivote)
+		if (array[j] <= pivot)
 		{
-			swap(i, j);
-			if (j != i)
+			i++;
+			swap(&array[i], &array[j]);
+			if (i != j)
 				print_array(array, size);
-			i += 1;
 		}
-		j += 1;
+		j++;
 	}
-	swap(i, Pivote);
-	if (i != j)
+	swap(&array[i + 1], &array[high]);
+	if (array[i + 1] != array[high])
 		print_array(array, size);
-
-	return (i - array);
+	return (i + 1);
 }
-
 /**
- * quickSort - quick sort implementation
+ * _quicksort - quick sort implementation
  * @array: Data structure.
- * @lower: Index lower
+ * @low: Index low
  * @high: Index high
  * @size: Size Data structure.
  */
-
-void quickSort(int *array, int *lower, int *high, int size)
+void _quicksort(int *array, int low, int high, size_t size)
 {
-	if (lower < high)
-	{
-		int Particion = partition(array, lower, high, size);
 
-		if (Particion)
-			quickSort(array, lower, array + (Particion - 1), size);
-		quickSort(array, array + (Particion + 1), high, size);
+	if (low < high)
+	{
+		int part = partition(array, low, high, size);
+
+		_quicksort(array, low, part - 1, size);
+		_quicksort(array, part + 1, high, size);
 	}
 }
 /**
- * quick_sort - function that sorts an array of integers in ascending
+ * quick_sort -  function that sorts an array of integers in ascending
  *               order using the Quick sort algorithm
  * @array: data structure that the function is applied to
  * @size: Data structure size
  */
-
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
+	if (!array  || size < 2)
 		return;
-
-	quickSort(array, array, array + (size - 1), size);
+	_quicksort(array, 0, size - 1, size);
 }
