c731e7ddbd7f3a6201095dcd55bd5878868616f1
apla02 <1873@holbertonschool.com>
2020-10-20 11:21:31 -0500

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorting algorithm of insertion
+ * @list: pointer to the list structure
+ * Return: node
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *key = NULL;
+	listint_t *aux = NULL;
+
+	if (!(list) || !(*list) || !(*list)->next)
+		return;
+	key = (*list)->next;
+	while (key)
+	{
+		aux = key->prev;
+		while (aux && aux->n > key->n)
+		{
+			aux->next = key->next;
+			if (key->next)
+				key->next->prev = aux;
+			key->prev = aux->prev;
+			if(aux->prev)
+				aux->prev->next = key;
+			else
+				*list = key;
+			key->next = aux;
+			aux->prev = key;
+			aux = key->prev;
+			print_list(*list);
+		}
+		key = key->next;
+	}
+}
